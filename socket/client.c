4929f26cacf5938ebc9604a31c6a098c88b16864
allenli <allenli-@pku.edu.cn>
2021-07-13 13:19:02 +0800

    根据实例代码的完成版socket通信 server.c-服务器 client.c-客户端

--- socket/client.c
+++ socket/client.c
@@ -1,8 +1,18 @@
 #include<stdio.h>
 #include<sys/socket.h>
+#include<arpa/inet.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<netdb.h>
+#include<sys/time.h>
+#include<netinet/in.h>
+/*
 int main(int argc,char *argv[])
 {
     int socket_desc;
+    struct sockaddr_in server;
     socket_desc=socket(AF_INET,SOCK_STREAM,0);
     //Address Family-AF_INET  Type-SOCK_STREAM   Protocol-0
     
@@ -10,5 +20,74 @@ int main(int argc,char *argv[])
     {
         printf("Could not create socket");
     }
+    server.sin_addr.s_addr=inet_addr("172.17.170.179");
+    server.sin_family=AF_INET;
+    server.sin_port=htons(PORT);
 
+    if(connect(socket_desc,(struct sockaddr *)&server,sizeof(server))<0)
+    {
+        puts("connect error");
+        return 1;
+    }
+    puts("connected");
+
+    char *message="My First http\n";
+    if(send(socket_desc,message,strlen(message),0)<0)
+    {
+        puts("send failed");
+        return 1;
+    }
+    puts("Data Send\n");
+
+    char server_reply[2000];
+    if(recv(socket_desc,server_reply,2000,0)<0)
+    {
+        puts("recv failed");
+    }
+    puts("Reply received\n");
+    puts(server_reply);
+
+    close(socket_desc);
+
+    return 0;
+}
+*/
+int main(int argc,char *argv[])
+{
+    char recvbuff[100];
+    int sockfd;
+    struct addrinfo hints,*si;
+    int rv;
+    /*
+    if(argc!=2){
+        printf("Usage: %s ip\n",argv[0]);
+        exit(0);
+    }
+    */
+    memset(&hints,0,sizeof(hints));
+    hints.ai_family=AF_INET;
+    hints.ai_socktype=SOCK_STREAM;
+
+    if((rv=getaddrinfo(argv[1],"5000",&hints,&si))!=0){
+        fprintf(stderr,"getaddrinfo: %s\n",gai_strerror(rv));
+        exit(1);
+    }
+
+    if((sockfd=socket(si->ai_family,si->ai_socktype,si->ai_protocol))==-1){
+        perror("socket");
+        exit(1);
+    }
+
+    if(connect(sockfd,si->ai_addr,si->ai_addrlen)==-1){
+        perror("connect");
+        exit(1);
+    }
+    freeaddrinfo(si);
+
+    if(recv(sockfd,recvbuff,sizeof(recvbuff),0)==-1){
+        perror("recv");
+    }
+    printf("%s\n",recvbuff);
+    close(sockfd);
+    return 0;
 }
--- socket/server.c
+++ socket/server.c
@@ -1,1 +1,106 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<netdb.h>
+#include<sys/time.h>
+#include<netinet/in.h>
+#define PORT 8888
+/*
+int main(char argc,char *argv[])
+{
+    int socket_desc,new_socket,c;
+    struct sockaddr_in server,client;
+
+    if(socket_desc==-1)
+    {
+        printf("Could not create socket");
+        return 1;
+    }
+
+    server.sin_family=AF_INET;
+    server.sin_addr.s_addr=INADDR_ANY;
+    server.sin_port=htons(PORT);
+
+    //Bind
+    if(bind(socket_desc,(struct sockaddr *)&server,sizeof(server))<0)
+    {
+        puts("bind failed");
+        return 1;
+    }
+    puts("bind done");
+
+    //Listen
+    listen(socket_desc,3);
+    
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c=sizeof(struct sockaddr_in);
+    new_socket=accept(socket_desc,(struct sockaddr *)&client,(socklen_t*)&c);
+    if(new_socket<0)
+    {
+        perror("accept failed");
+        return 1;
+    }
+    puts("Connection accepted");
+
+    char *message="Hello Client, I've received your connection.But I have to go now,bye\n";
+    write(new_socket,message,strlen(message));
+    return 0;
+}
+*/
+int main(int argc,char *argv[])
+{
+    int listensock,connsock;
+    struct addrinfo hints,*si;
+    const char buff[]="Hello! This is my first message to you!\n";
+    char recvbuf[100];
+    int rv;
+
+    memset(&hints,0,sizeof(hints));
+    hints.ai_family=AF_INET;
+    hints.ai_socktype=SOCK_STREAM;
+    hints.ai_flags=AI_PASSIVE;
+
+    if((rv=getaddrinfo(NULL,"5000",&hints,&si))!=0)
+    {
+        fprintf(stderr,"getaddrinfo: %s\n",gai_strerror(rv));
+        exit(1);
+    }
+    
+    if((listensock=socket(si->ai_family,si->ai_socktype,si->ai_protocol))==-1)
+    {
+        perror("socket");
+        exit(1);
+    }
+
+    rv=1;
+    if(setsockopt(listensock,SOL_SOCKET,SO_REUSEADDR,&rv,sizeof(int))==-1)
+    {
+        perror("setsockopt");
+        exit(1);
+    }
+
+    if(bind(listensock,si->ai_addr,si->ai_addrlen)==-1){
+        close(listensock);
+        perror("bind");
+        exit(1);
+    }
+    freeaddrinfo(si);
+
+    if(listen(listensock,10)==-1){
+        perror("listen");
+        exit(1);
+    }
+
+    connsock=accept(listensock,NULL,NULL);
+    close(listensock);
+    if(send(connsock,buff,sizeof(buff),0)==-1){
+        perror("send");
+    }
+    close(connsock);
+    return 0;
+}
